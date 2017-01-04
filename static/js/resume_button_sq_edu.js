6cef59596d6bd7a181ec58cf06ac40027251696b
stsupertam <supmantam@gmail.com>
2017-01-05 01:18:13 +0700

    finsish resume button function 65%

---
+++ static/js/resume_button_sq_edu.js
@@ -0,0 +1,64 @@
+function plus_button(type, counter){
+    temp = type.split('-');
+    copy_id = "li-" + type + '-' + counter;
+    origin = "#li-" + type + '-' + (counter -1);
+    target = ".input." + temp[0];
+    var html = $(origin).clone();
+    html.attr("id", copy_id);
+    $(target).append(html);
+}
+
+function minus_button(type, counter){
+    var id = type + counter;
+    $(id).remove();
+}
+
+var counter_skill = 1;
+var counter_qualification = 1;
+var counter_joblist = [];
+
+$(function() {
+    $(".plus-function").click(function(){
+        var parent_class = $(this).parent().attr("class").split(' ');
+        if(parent_class[1] == "skill"){
+            counter_skill += 1;
+            plus_button("skill", counter_skill);
+        }
+        else if(parent_class[1] == "qualification"){
+            counter_qualification += 1;
+            plus_button("qualification", counter_qualification);
+        }
+        else if(parent_class[1] == "joblist"){
+            if(counter_joblist[parent_class[2]] == null){
+                counter_joblist[parent_class[2]] = 1;
+            }
+            counter_joblist[parent_class[2]] += 1;
+            joblist = "joblist-" + parent_class[2]; 
+            plus_button(joblist, counter_joblist[parent_class[2]]);
+        }
+    });
+    $(".minus-function").click(function(){
+        var parent_class = $(this).parent().attr("class").split(' ');
+        if(parent_class[1] == "skill"){
+            if(counter_skill >= 2){
+                minus_button("#li-skill-", counter_skill);
+                counter_skill -= 1;
+            }
+        }
+        else if(parent_class[1] == "qualification"){
+            if(counter_qualification >= 2){
+                minus_button("#li-qualification-", counter_qualification);
+                counter_qualification -= 1;
+            }
+        }
+        else if(parent_class[1] == "joblist"){
+            if(counter_joblist[parent_class[2]] != null){
+                if(counter_joblist[parent_class[2]] >= 2){
+                    joblist = "#li-joblist-" + parent_class[2] + "-";
+                    minus_button(joblist, counter_joblist[parent_class[2]]);
+                    counter_joblist[parent_class[2]] -= 1;
+                }
+            }
+        }
+    });
+});
